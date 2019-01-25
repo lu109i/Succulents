698b7fef46c7c2b38cb176752d4934abeaf1f470
lulu <weidaily303@gmail.com>
2019-01-25 12:20:33 +0800

    Initial Commit

---
+++ Succulents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Succulents
+//
+//  Created by lulu on 2019/1/25.
+//  Copyright © 2019 lulu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
